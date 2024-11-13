82007e760e930f00793c43482596e8f9d1e707ae
Ian Graham <ian@iangraham.io>
2024-11-13 06:49:12 -0800

    Added additional tests

--- tests/Feature/LeadTest.php
+++ tests/Feature/LeadTest.php
@@ -18,3 +18,23 @@ it('returns the correct number of leads when limit is passed', function(){
     $leadData = Lead::search(null, null, 1);
     expect(count($leadData->items()))->toBe(1);
 });
+
+it('returns the correct number of leads when query is passed', function(){
+    $leads = Lead::factory()->count(3)->create([
+        'lead_status_id' => 1
+    ]);
+    $leadData = Lead::search($leads->first()->name, null, 100);
+    expect(count($leadData->items()))->toBe(1);
+});
+
+it('returns the correct number of leads when passing status', function(){
+
+    $leadStatuses = LeadStatus::factory()->count(3)->create();
+    $leadStatuses->each(function ($status) {
+        Lead::factory()->create(['lead_status_id' => $status->id]);
+    });
+
+    $leadData = Lead::search(null, $leadStatuses->first()->id, 100);
+    expect(count($leadData->items()))->toBe(1);
+
+});
